579b6c190b88042fdb178a387065a74892e4c377
guyett92 <aaronguyett@yahoo.com>
2021-11-19 03:07:04 -0800

    adds coinbase commerce

--- pages/order.js
+++ pages/order.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Layout from "../components/Layout";
+import { Container, Row, Col } from "reactstrap";
 
 const Order = () => {
   // State
@@ -102,6 +103,42 @@ const Order = () => {
   return (
     <Layout pageTitle="eluno | Order">
       <Header />
+      <section className="section">
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col className="center">
+              <h1>Hoodie</h1>
+              <div className="image-container-no-overflow">
+                <img src="/images/monstera.jpg" alt="" />
+              </div>
+              <div>
+                <a
+                  class="buy-with-crypto"
+                  href="https://commerce.coinbase.com/checkout/20dc7f60-3272-406e-a092-3e2c4f6f1fca"
+                >
+                  Buy with Crypto
+                </a>
+                <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807"></script>
+              </div>
+            </Col>
+            <Col className="center">
+              <h1>Shirt</h1>
+              <div className="image-container-no-overflow">
+                <img src="/images/eva.jpg" alt="" />
+              </div>
+              <div>
+                <a
+                  className="buy-with-crypto"
+                  href="https://commerce.coinbase.com/checkout/4220b634-dc1b-48fc-ad86-38c540823d24"
+                >
+                  Buy with Crypto
+                </a>
+                <script src="https://commerce.coinbase.com/v1/checkout.js?version=201807"></script>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
       <Footer />
     </Layout>
   );
