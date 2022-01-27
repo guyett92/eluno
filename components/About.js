c643b64406fb4e2bcbb5fe08b78bd01644a55201
guyett92 <aaronguyett@yahoo.com>
2022-01-26 23:30:55 -0800

    updates team

--- components/About.js
+++ components/About.js
@@ -8,20 +8,20 @@ const About = () => {
         <h1 className="text-dark title most-titles text-center">the squad</h1>
         <Row className="justify-content-center">
           <Col className="space-center">
-            <div className="image-container">
-              <img src="/images/jonah.png" alt="jonah" width={400} />
-            </div>
-            <h4 className="text-dark text-center">Jonah</h4>
-          </Col>
-          <Col className="space-center">
-            <div className="image-container">
-              <img src="/images/aaron.png" alt="aaron" width={400} />
+            <div className="image-container aaron">
+              <img src="/images/AARON.ELUNO.jpg" alt="aaron" height={400} />
             </div>
             <h4 className="text-dark text-center">Aaron</h4>
           </Col>
           <Col className="space-center">
-            <div className="image-container">
-              <img src="/images/anayo.png" alt="anayo" width={400} />
+            <div className="image-container jonah">
+              <img src="/images/JONAH.SB.jpg" alt="jonah" height={400} />
+            </div>
+            <h4 className="text-dark text-center">Jonah</h4>
+          </Col>
+          <Col className="space-center">
+            <div className="image-container anayo">
+              <img src="/images/ANAYO.jpg" alt="anayo" height={400} />
             </div>
             <h4 className="text-dark text-center">Anayo</h4>
           </Col>
